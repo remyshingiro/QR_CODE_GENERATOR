c0c634fae1902be5f7d3b716ffb1a738457a6931
Remy <shingiroremy303@gmail.com>
2025-08-16 16:49:53 +0200

    Add error handling and animation for QR code input validation

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ let qrImg = document.getElementById("qrImg");
 let qrText = document.getElementById("qrText");
 
 const generateCode = () => {
-    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
-    imgBox.classList.add("show-img");
+    if(qrText.value.trim().length > 0) {
+        qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show-img");
+    } else {
+        qrText.classList.add("error");
+        setTimeout(()=>{
+            qrText.classList.remove('error');
+        }, 1000);
+    }
+    
 }
